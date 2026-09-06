87e5bd1ea6d4f02b878094f23976473dbe18f7aa
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-06 21:46:47 +0100

    ft: add sign up component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import chat from "../assets/chat.png";
 import apple from "../assets/apple-logo.png";
 import google from "../assets/google.png";
 import hero from "../assets/hero.jpg";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
@@ -9,11 +10,13 @@ function Login() {
       <section className="mx-3 md:mx-8">
         <div className="flex gap-1 py-4 mx-5">
           <img src={chat} className="w-7" alt="" />
-          <h1 className="text-[#352477] font-sans text-lg font-semibold">NU<span className="text-[#597e16] text-xl">D</span>GE</h1>
+          <h1 className="text-[#352477] font-sans text-lg font-semibold">
+            NU<span className="text-[#597e16] text-xl">D</span>GE
+          </h1>
         </div>
 
         <div className=" grid grid-cols-1 md:grid-cols-2 gap-28 mx-5 my-6">
-          <div >
+          <div>
             <div>
               <button className="flex gap-3 overflow-y-hidden items-center rounded-3xl px-3 py-2 my-2 w-full text-sm md:text-lg border border-gray-700">
                 <img className="w-4 " src={google} alt="Google logo" />
@@ -30,26 +33,32 @@ function Login() {
             <form action="">
               <input
                 className="border border-gray-700 rounded-lg px-3 py-3 w-full my-2"
-                type="text"
+                type="email or username"
                 placeholder="Email or Username"
                 required
               />
 
               <input
                 className="border border-gray-700 rounded-lg px-3 py-3 w-full my-2"
-                type="text"
+                type="password"
                 placeholder="Password"
                 required
               />
 
-              <button className="flex gap-3 items-center rounded-3xl bg-[#352477] text-white justify-center px-3 py-2  w-full my-6 text-sm md:text-lg border border-gray-700">
+              <button
+                type="submit"
+                className="flex gap-3 items-center rounded-3xl bg-[#352477] text-white justify-center px-3 py-2  w-full my-6 text-sm md:text-lg border border-gray-700"
+              >
                 Log in
               </button>
 
-              <p className="text-xs md:text-sm text-gray-700 text-center  ">
-                By continuing you agree to our Terms and Condition, Privacy and
-                Use of Cookies.
-              </p>
+              <div className="flex gap-1 items-center">
+                <input type="checkbox" className="w-5 " required />
+                <p className="text-xs md:text-sm text-gray-700 text-center  ">
+                  By continuing you agree to our Terms and Condition, Privacy
+                  and Use of Cookies.
+                </p>
+              </div>
 
               <p className="text-center hover:text-blue-600 text-sm text-gray-700 font-semibold py-3">
                 Forgot password?
@@ -58,9 +67,9 @@ function Login() {
 
             <div className="text-center py-5 text-sm text-gray-700">
               <p>Dont have an account yet?</p>
-              <p className="font-semibold underline hover:text-blue-600">
+              <Link to= "signup" className="font-semibold underline hover:text-blue-600">
                 Sign Up
-              </p>
+              </Link>
             </div>
           </div>
 
@@ -72,7 +81,6 @@ function Login() {
             <img className="object-contain" src={hero} alt="" />
           </div>
         </div>
-
 
         <div className="text-xs text-gray-700 flex flex-wrap gap-x-2 gap-y-1 justify-center mx-5 mt-24">
           <p>About</p>
