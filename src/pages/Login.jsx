bd1d36a62c7e33a875a25196a91c128a147de0b4
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-06 20:52:44 +0100

    ft: sign up button add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,37 +9,62 @@ function Login() {
 
       <p>Real people. Real experiences</p>
 
-      <div>
-        <button className="flex gap-3 overflow-y-hidden items-center rounded-3xl px-3 py-2 my-2 w-full text-sm md:text-lg border border-gray-700">
-          <img className="w-4 " src={google} alt="Google logo" />
-          <span>Sign in with Google</span>
-        </button>
-        <button className="flex gap-3 items-center rounded-3xl  px-3 py-2 w-full my-2 text-sm md:text-lg border border-gray-700">
-          <img className="w-5 " src={apple} alt="apple logo" />
-          <span>Sign in with Apple</span>
-        </button>
-      </div>
+      <section>
+        <div>
+          <div>
+            <button className="flex gap-3 overflow-y-hidden items-center rounded-3xl px-3 py-2 my-2 w-full text-sm md:text-lg border border-gray-700">
+              <img className="w-4 " src={google} alt="Google logo" />
+              <span>Sign in with Google</span>
+            </button>
+            <button className="flex gap-3 items-center rounded-3xl  px-3 py-2 w-full my-2 text-sm md:text-lg border border-gray-700">
+              <img className="w-5 " src={apple} alt="apple logo" />
+              <span>Sign in with Apple</span>
+            </button>
+          </div>
 
-      <p className="text-center font-semibold text-gray-700 ">...Or...</p>
+          <p className="text-center font-semibold text-gray-700 ">...Or...</p>
 
-      <form action="">
-        
-        <input className="border border-gray-700 rounded-lg px-3 py-3 w-full my-2" type="text" placeholder="Email or Username" required />
+          <form action="">
+            <input
+              className="border border-gray-700 rounded-lg px-3 py-3 w-full my-2"
+              type="text"
+              placeholder="Email or Username"
+              required
+            />
 
-       
-        <input className="border border-gray-700 rounded-lg px-3 py-3 w-full my-2" type="text" placeholder="Password" required />
+            <input
+              className="border border-gray-700 rounded-lg px-3 py-3 w-full my-2"
+              type="text"
+              placeholder="Password"
+              required
+            />
 
-        <button  className="flex gap-3 items-center rounded-3xl bg-[#352477] text-white justify-center px-3 py-2  w-full my-6 text-sm md:text-lg border border-gray-700">Log in</button>
+            <button className="flex gap-3 items-center rounded-3xl bg-[#352477] text-white justify-center px-3 py-2  w-full my-6 text-sm md:text-lg border border-gray-700">
+              Log in
+            </button>
 
-        <div className="flex gap-2 justify-center items-center">
-          <p className="text-center text-sm text-gray-700 "> Forgot password?</p>
-          <p className="text-blue-600 text-sm underline">link</p>
+            <p className="text-sm text-gray-700 text-center  ">
+              By continuing you agree to our Terms and Condition, Privacy and
+              Use of Cookies.
+            </p>
+
+            <p className="text-center hover:text-blue-600 text-sm text-gray-700 font-semibold py-3">
+              Forgot password?
+            </p>
+          </form>
+
+          <div className="text-center py-5 text-sm text-gray-700">
+            <p>Dont have an account yet?</p>
+            <p className="font-semibold underline hover:text-blue-600">
+              Sign Up
+            </p>
+          </div>
         </div>
-      </form>
 
-
-<p>Dont have an account yet?</p>
-      <p>Sign Up</p>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </section>
     </>
   );
 }
