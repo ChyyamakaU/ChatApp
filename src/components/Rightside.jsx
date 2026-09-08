b25f028e9526267659f34cc7390b99a0ff3a7e14
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-08 21:41:41 +0100

    ft: add logout button and styling

--- src/components/Rightside.jsx
+++ src/components/Rightside.jsx
@@ -1,10 +1,11 @@
 import friends from "../assets/friends.jpg";
 import young from "../assets/Young.jpg";
+// import Link from "react-router-dom";
 
 function Rightside() {
   return (
     <>
-      <section className="col-span-1">
+      <section className="col-span-1 relative">
         <div className="bg-white h-screen overflow-y-scroll">
           <div className="shrink-0  border-b border-gray-300 px-4 py-5">
             <img
@@ -17,22 +18,52 @@ function Rightside() {
               <p className="text-lg font-semibold">Jane Doe</p>
               <div className="w-2 h-2 rounded-full bg-green-500"></div>
             </div>
-            <p className="text-xs md:text-md opacity-75 py-2 font-medium ">Hi Lovelies, I am Jane Doe and I am new to Nudge</p>
+            <p className="text-xs md:text-md opacity-75 py-2 font-medium ">
+              Hi Lovelies, I am Jane Doe and I am new to Nudge
+            </p>
           </div>
           <hr />
           <div>
-
             <h3 className="text-lg font-semibold p-1">Media</h3>
             <div className="grid grid-cols-3 gap-2 p-2 overflow-y-scroll max-h-40">
-              <img src={young} className="w-50 rounded-2xl object-cover" alt="" />
-              <img src={young} className="w-50 rounded-2xl object-cover" alt="" />
-              <img src={young} className="w-50 rounded-2xl object-cover" alt="" />
-              <img src={young} className="w-50 rounded-2xl object-cover" alt="" />
-              <img src={young} className="w-50 rounded-2xl  object-cover" alt="" />
-              <img src={young} className="w-50 rounded-2xl  object-cover" alt="" />
+              <img
+                src={young}
+                className="w-50 rounded-2xl object-cover"
+                alt=""
+              />
+              <img
+                src={young}
+                className="w-50 rounded-2xl object-cover"
+                alt=""
+              />
+              <img
+                src={young}
+                className="w-50 rounded-2xl object-cover"
+                alt=""
+              />
+              <img
+                src={young}
+                className="w-50 rounded-2xl object-cover"
+                alt=""
+              />
+              <img
+                src={young}
+                className="w-50 rounded-2xl  object-cover"
+                alt=""
+              />
+              <img
+                src={young}
+                className="w-50 rounded-2xl  object-cover"
+                alt=""
+              />
             </div>
           </div>
-          <button>Log Out</button>
+          <button
+            type="submit"
+            className="absolute bottom-6  translate-x-1/2 rounded-3xl bg-[#352477] text-white justify-center px-3 py-2  w-3/6 my-8 text-sm md:text-lg border border-gray-700"
+          >
+            Sign Out
+          </button>
         </div>
       </section>
     </>
